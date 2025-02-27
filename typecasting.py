eaad8cc9799a685c9f48d06bde9fad4c15c0c979
dhreetirayyy <dkumar.deo@gmail.com>
2025-02-26 17:10:38 -0800

    Learned Typecasting

---
+++ typecasting.py
@@ -0,0 +1,15 @@
+name = "Madison"
+age = 15
+weight = 83.5
+is_student = False
+print("Name:", name)
+print("Data Type of Name is", type(name))
+print("Age:", age)
+print("Data Type of Age is", type(age))
+print("Weight:", weight)
+print("Data Type of Weight is", type(weight))
+print("is_student:", is_student)
+print("Data Type of is_student is", type(is_student))
+age = str(age)
+print(age)
+print("Data Type of Age", type(age))
